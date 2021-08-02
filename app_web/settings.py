dc2847ceb652dbbcc196c66992c315902621046f
Samuel Fernandes <samueldiablo73@gmail.com>
2021-08-02 11:44:33 -0300

    add sqlite

--- app_web/settings.py
+++ app_web/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'app_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
