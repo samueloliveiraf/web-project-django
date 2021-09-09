b250fef585d7ecc5f199532ad943a7fb8cbb43ba
samuel <samueldiablo73@gmail.com>
2021-09-08 21:53:22 -0300

    ajustes

--- app_web/settings.py
+++ app_web/settings.py
@@ -132,6 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
 
