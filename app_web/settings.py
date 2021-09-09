b119c44f1d66d6fb326f2fd03e9ff6610352ebdb
samuel <samueldiablo73@gmail.com>
2021-09-08 21:58:02 -0300

    ajustes

--- app_web/settings.py
+++ app_web/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
